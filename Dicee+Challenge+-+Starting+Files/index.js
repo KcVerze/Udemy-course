9cf7da45925826c61faefa3886db1ef184311831
KcVerze <kcverze@gmail.com>
2025-06-09 01:33:07 +0100

    dice update

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -2,7 +2,19 @@ function player(player_class) {
     var randomNumber = Math.floor(Math.random() * 6) + 1;
     var image = document.querySelector(`.${player_class}`);
     image.setAttribute("src", `./images/dice${randomNumber}.png`);
+
+    return randomNumber;
 }
 
-player("img1");
-player("img2");
+const randomNumber1 = player("img1");
+const randomNumber2 = player("img2");
+
+if (randomNumber1 > randomNumber2){
+        document.querySelector("h1").textContent = "Player 1 Wins";
+    }
+    else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").textContent = "Player 2 Wins";
+    }
+    else {
+        document.querySelector("h1").textContent = "Drew";
+    }
