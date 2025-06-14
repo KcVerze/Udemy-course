fb05630754078a7633ef1f44073723fb64e63acc
KcVerze <kcverze@gmail.com>
2025-06-14 23:16:45 +0100

    update

--- step2/section 2/3.4 Middleware/index4.js
+++ step2/section 2/3.4 Middleware/index4.js
@@ -1,7 +1,36 @@
 import express from "express";
+//the middle ware import
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var bandName = "";
+
+// the middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
+// to generate the band name
+function bandNameGenerator(req, res, next){
+  console.log(req.body);
+  bandName = req.body["street"] + req.body["pet"];
+  next();
+}
+
+app.use(bandNameGenerator);
+
+// to send back the index html resource
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// the form result
+app.post("/submit",(req, res) =>{   
+    res.send(`<h1>Your band name is:</h1><h2>${bandName}</h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
