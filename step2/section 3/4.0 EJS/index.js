7585f74fe6240bc56e0579697aef75dedc74d36a
KcVerze <kcverze@gmail.com>
2025-06-15 22:58:39 +0100

    update 4

--- step2/section 3/4.0 EJS/index.js
+++ step2/section 3/4.0 EJS/index.js
@@ -4,9 +4,21 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+    //const today = new Date("June 24,2025 11:13:00");
+    const today = new Date();
+    const day = today.getDay();
+
+    //console.log(day);
+    let type = "a weekday";
+    let adv = "work hard";
+
+    if (day === 0 || day === 6){
+        type = "the weekend";
+        adv = "work even harder";
+    }
     res.render("index.ejs", {
-        dayType: "a weekday", 
-        advice: "work hard"});
+        dayType: type, 
+        advice: adv,});
 });
 
 app.listen(port, () => {
